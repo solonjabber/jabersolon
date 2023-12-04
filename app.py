70a5d709d1951917ee406504a6f63133e0c1f807
solonjabber <152863757+solonjabber@users.noreply.github.com>
2023-12-04 06:59:10 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/sobre')
+def sobre():
+    return render_template("sobre.html")
+
+
+@app.route('/compras')
+def comprar():
+    return render_template("compras.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
